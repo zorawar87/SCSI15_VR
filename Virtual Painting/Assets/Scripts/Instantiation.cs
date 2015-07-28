c5a15c2e4f808ff4bbb0d883ddc964056943c4fe
zorawar87 <zorawar87@gmail.com>
2015-07-28 15:53:10 -0500

    Instantiation works perfectly; gesture recognition (for screen tap) added, not tested

--- Virtual Painting/Assets/Scripts/Instantiation.cs
+++ Virtual Painting/Assets/Scripts/Instantiation.cs
@@ -2,87 +2,96 @@
 using System.Collections;
 using Leap;
 
-public class Instantiation : MonoBehaviour {
-	//public Rigidbody projectile;
-	public GameObject projectile;
-	public int limiter=5;
+public class Instantiation : MonoBehaviour
+{
+	public GameObject cube;
+	public int limiter = 5;
+	public float boxSeparation = 4f;
+	
+	HandController leap;
+	HandModel[] hands;
 	Controller controller;
 	//Rigidbody clone;
+	
+	void Start ()
+	{
+		leap = GetComponent<HandController> ();
+		controller = leap.GetLeapController ();
 
-	void Start(){
-		controller = new Controller();
-		/*controller.EnableGesture(Gesture.GestureType.TYPE_SWIPE);
-		controller.Config.SetFloat("Gesture.Swipe.MinLength", 20.0f);
-		controller.Config.SetFloat("Gesture.Swipe.MinVelocity", 50f);
-		controller.Config.Save();*/
+		controller.EnableGesture (Gesture.GestureType.TYPE_SCREEN_TAP);
+		controller.Config.SetFloat ("Gesture.ScreenTap.MinForwardVelocity", 0.5f);
+		controller.Config.SetFloat ("Gesture.ScreenTap.HistorySeconds", 0.5f);
+		controller.Config.SetFloat ("Gesture.ScreenTap.MinDistance", 1.0f);
+		controller.Config.Save ();
+	}
+	
+	void Update ()
+	{
+		HandModel[] hands = leap.GetAllGraphicsHands ();
+		GestureRecognition (hands);
 	}
 
-	void Update() {
-		//Leap Motion code
-		Frame frame = controller.Frame(); // controller is a Controller object
-		HandList hands = frame.Hands;
-		Hand rhand = frame.Hands.Rightmost;
-		Hand lhand = frame.Hands.Leftmost;
+	void CreateClones (HandModel[] hands)
+	{
 
-		//Normal code
-		print ("start");
-		if (rhand.IsValid && lhand.IsValid) {
-			if (rhand.IsRight) {
-				print ("Right Hand");
-				isRH (rhand);
-			} else if (lhand.IsLeft) {
-				print ("LEft Hand");
-				isLH (lhand);
-			} else{
-				print ("MOTHER OF GOD HOW MANY HANDS DO YOU HAVE?");
-			}
-		} else {
-			print("leap says fu");
-		}
-
-	}
-
-	void isRH(Hand rhand){
-		
-		//Instantiate (projectile, position, transform.rotation);
-		//CloneCode
-		GameObject[] clones = GameObject.FindGameObjectsWithTag("Clone");
+		GameObject[] clones = GameObject.FindGameObjectsWithTag ("Clone");
 		if (clones.Length < limiter) {
-			print ("RHClone");
-			//Vector3 position = new Vector3 (rhand.PalmNormal.x, rhand.PalmNormal.y, rhand.PalmNormal.z);
-			for (int i=0; i<=clones.Length;i++){
-				Vector3 position = new Vector3 (rhand.PalmPosition.x/1000, rhand.PalmPosition.y/1000, rhand.PalmPosition.z/1000); //PalmNormal or PalmPosition
-				//Vector3 position = new Vector3 (rhand.PalmPosition.z/1000, 0, rhand.PalmPosition.x/1000); //PalmNormal or PalmPosition
-				//Vector3 position = new Vector3 (-rhand.PalmPosition.z/1000, 0, 00); //PalmNormal or PalmPosition
-				//Vector3 position = new Vector3 (-rhand.PalmPosition.z/1000, 0, 00); //PalmNormal or PalmPosition
-				position= transform.TransformPoint(position);
-				Debug.Log ("Defpos"+rhand.PalmPosition);
-				Debug.Log ("PalmPos"+position);
-				if(i != 0){
-					print("i!=0");
-					if ( 
-					     (clones[i].transform.position.x  <= clones[i-1].transform.position.x *1.5f) ||
-						 (clones[i].transform.position.y  <= clones[i-1].transform.position.y *1.5f) ||
-						 (clones[i].transform.position.z  <= clones[i-1].transform.position.z *1.5f)
-					 	){
-						print ("Shouldn't work");
+			for (int i=0; i<=clones.Length; i++) {
+				//!!!!GETS POSITION OF FIRST HAND!!!!!
+				Vector3 position = hands [0].GetPalmPosition ();
+
+				if (i != 0) {
+					Debug.Log ("i!=0");
+					if (
+					    (clones [i].transform.position.x <= clones [i - 1].transform.position.x * boxSeparation) &&
+						(clones [i].transform.position.y <= clones [i - 1].transform.position.y * boxSeparation) &&
+						(clones [i].transform.position.z <= clones [i - 1].transform.position.z * boxSeparation)
+					    ) {
+						Debug.Log ("Shouldn't work");
 					}
-				} else{
-					print("Instant");
-					Instantiate (projectile, position, transform.rotation);
+				} else {
+					print ("FromInstant");
+					Instantiate (cube, position, transform.rotation);
 				}	
 			}
-			print ("done");
-		}else{
-			print ("fail"+GameObject.FindGameObjectsWithTag("Clone").Length);
-			for(int i=0; i<limiter;i++){
-				Destroy(GameObject.FindGameObjectWithTag("Clone"));
+			Debug.Log ("done");
+		} else {
+			Debug.Log ("fail" + GameObject.FindGameObjectsWithTag ("Clone").Length);
+			for (int i=0; i<limiter; i++) {
+				Destroy (GameObject.FindGameObjectWithTag ("Clone"));
 			}
 		}
 	}
 
+	void GestureRecognition (HandModel[] hands)
+	{
+		Frame frame = controller.Frame ();
+		GestureList gesturesInFrame = frame.Gestures ();
+		bool CloneGenerationActive = false;
 
-	void isLH(Hand lhand){
-
+		for (int i = 0; i <= gesturesInFrame.Count; i++) {
+			Gesture gesture = gesturesInFrame [i];
+			
+			if (gesture.Type == Gesture.GestureType.TYPE_SCREEN_TAP) {
+				ScreenTapGesture screentapGesture = new ScreenTapGesture (gesture);
+				Debug.Log ("scrtap");
+				if (CloneGenerationActive) {
+					CloneGenerationActive = false;
+				} else if (CloneGenerationActive == false) {
+					CloneGenerationActive = true;
+				} else {
+					Debug.Log ("what condition is this?");
+				}
+			} else if (gesture.Type == Gesture.GestureType.TYPE_INVALID) {
+				print ("invalid");
+			}
+		}
+		if (CloneGenerationActive) {
+			CreateClones (hands);
+			print ("CreateClonesCalled");
+		} else {
+			print ("CreateClonesNOTCalled");
+		}
+		print ("called");
 	}
 }
