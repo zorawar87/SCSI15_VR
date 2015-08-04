17c4659a991ec57f6660ae5631ef043ea99048a4
zorawar87 <zorawar87@gmail.com>
2015-08-04 15:50:00 -0500

    cleanup; improvements; textures added -- needs work; maneuvering

--- Virtual Painting/Assets/Scripts/Instantiation.cs
+++ Virtual Painting/Assets/Scripts/Instantiation.cs
@@ -3,16 +3,10 @@ using System.Collections;
 using System.Collections.Generic;
 using Leap;
 
-public class Instantiation : MonoBehaviour
-{
-	public GameObject cube;
-	public int limiter = 5;
-	public float boxSeparation = 4f;
-
+public class Instantiation : MonoBehaviour{
 	HandController leap;
 	HandModel[] hands;
 	Controller controller;
-	bool isCloneGenerationActive;
 	Frame frame;
 
 	Mesh dynaMesh;
@@ -23,20 +17,12 @@ public class Instantiation : MonoBehaviour
 	List<Vector2> uvCoords;
 	int whereInTheMesh;
 	bool handsInLastFrame;
-	public Material mat;
 
-	//Rigidbody clone;
+	public Material mat;
 	
 	void Start () {
-		isCloneGenerationActive = false;
 		leap = GetComponent<HandController> ();
 		controller = leap.GetLeapController ();
-
-		controller.EnableGesture (Gesture.GestureType.TYPE_SCREEN_TAP);
-		controller.Config.SetFloat ("Gesture.ScreenTap.MinForwardVelocity", 0.5f);
-		controller.Config.SetFloat ("Gesture.ScreenTap.HistorySeconds", 0.5f);
-		controller.Config.SetFloat ("Gesture.ScreenTap.MinDistance", 1.0f);
-		controller.Config.Save ();
 
 		dynaMesh = new Mesh();
 		dynaMesh.subMeshCount = 1;
@@ -47,6 +33,13 @@ public class Instantiation : MonoBehaviour
 		uvCoords = new List<Vector2>();
 		whereInTheMesh = 0;
 		handsInLastFrame = false;
+
+		//functionality unavailable
+		controller.EnableGesture (Gesture.GestureType.TYPE_SCREEN_TAP);
+		controller.Config.SetFloat ("Gesture.ScreenTap.MinForwardVelocity", 0.5f);
+		controller.Config.SetFloat ("Gesture.ScreenTap.HistorySeconds", 0.5f);
+		controller.Config.SetFloat ("Gesture.ScreenTap.MinDistance", 1.0f);
+		controller.Config.Save ();
 	}
 	
 	void Update () {
@@ -54,8 +47,8 @@ public class Instantiation : MonoBehaviour
 		hands = leap.GetAllGraphicsHands ();
 		if(hands.Length>0){
 			handsInLastFrame = true;
-			//onBothHandsDetected(hands);
-			DynamicMeshGenerator(hands);
+			onBothHandsDetected(hands);
+			//DynamicMeshGenerator(hands);
 		} else{
 			noHandsWhatToDo();
 		}
@@ -64,19 +57,16 @@ public class Instantiation : MonoBehaviour
 	void onBothHandsDetected(HandModel[] hands){
 		if(hands.Length==2){
 			DynamicMeshGenerator(hands);
-			print("boolean is"+ handsInLastFrame);
 		} else if(hands.Length==1){
-			print (hands.Length +"hands detected");
+			print (hands.Length +"hand(s) detected");
 		} else{
 			print ("ERROR");
-			print (hands.Length +" hands detected");
+			print (hands.Length +" hand(s) detected");
 		}
 	}
 
 	void noHandsWhatToDo(){
 		if (handsInLastFrame == true) {
-			print("FROM T bool is"+ handsInLastFrame);
-			
 			triangles.Clear();
 			whereInTheMesh=0;
 			dynaMesh.subMeshCount++;
@@ -89,16 +79,20 @@ public class Instantiation : MonoBehaviour
 		}
 	}
 
+
+
 	void DynamicMeshGenerator(HandModel[] hands){
 
 		Vector3 indexFPos = hands[0].fingers[(int)Finger.FingerType.TYPE_INDEX].GetTipPosition();
 		Vector3 thumbFPos = hands[0].fingers[(int)Finger.FingerType.TYPE_THUMB].GetTipPosition();
 
-		vertices.Add(indexFPos); whereInTheMesh++;
-		uvCoords.Add(new Vector2(0f, 0.0f));
-		vertices.Add(thumbFPos); whereInTheMesh++;
-		uvCoords.Add(new Vector2(0f, 1f));
+		vertices.Add(indexFPos);
+		whereInTheMesh++;
+		uvCoords.Add(new Vector2(0f, 0f));
 
+		vertices.Add(thumbFPos);
+		whereInTheMesh++;
+		uvCoords.Add(new Vector2(1f, 1f));
 		
 		if(vertices.Count>3 && whereInTheMesh >3){
 			int a = vertices.Count-4;
@@ -112,7 +106,6 @@ public class Instantiation : MonoBehaviour
 			triangles.Add (c);
 			triangles.Add (d);
 			triangles.Add (a);
-			print ("triangle at ("+a+", "+d+", "+c+")");
 			
 			triangles.Add (a);
 			triangles.Add (b);
@@ -125,55 +118,16 @@ public class Instantiation : MonoBehaviour
 		}
 
 		dynaMesh.vertices = vertices.ToArray();
-		print ("dmc"+dynaMesh.subMeshCount);
 		dynaMesh.SetTriangles(triangles.ToArray(), dynaMesh.subMeshCount-1);
-		int z= dynaMesh.subMeshCount-1;
-		print ("dm-1"+z);
-		//dynaMesh.triangles = triangles.ToArray();
 		dynaMesh.uv = uvCoords.ToArray();
 		mFilter.mesh = dynaMesh;
-
 	}
 
-
-
-	//Used For CubeClones. Unused.
-	void CreateClones (HandModel[] hands){
-		
-		GameObject[] clones = GameObject.FindGameObjectsWithTag ("Clone");
-		if (clones.Length < limiter) {
-			for (int i=0; i<=clones.Length; i++) {
-				//!!!!GETS POSITION OF FIRST HAND!!!!!
-				Vector3 position = hands [0].GetPalmPosition ();
-				
-				if (i != 0) {
-					Debug.Log ("i!=0");
-					if (
-						(clones [i].transform.position.x <= clones [i - 1].transform.position.x * boxSeparation) &&
-						(clones [i].transform.position.y <= clones [i - 1].transform.position.y * boxSeparation) &&
-						(clones [i].transform.position.z <= clones [i - 1].transform.position.z * boxSeparation)
-						) {
-						Debug.Log ("Shouldn't work");
-					}
-				} else {
-					print ("FromInstant");
-					Instantiate (cube, position, transform.rotation);
-				}	
-			}
-			Debug.Log ("done");
-		} else {
-			Debug.Log ("fail" + GameObject.FindGameObjectsWithTag ("Clone").Length);
-			for (int i=0; i<limiter; i++) {
-				Destroy (GameObject.FindGameObjectWithTag ("Clone"));
-			}
-		}
-	}
-
-	//----------------------------------------------------------------------
-
-	//purge/fix the following
+	//----Unused---
+	bool isCloneGenerationActive;
 
 	void GestureRecognition (HandModel[] hands) {
+		isCloneGenerationActive=false;
 		GestureList gesturesInFrame = frame.Gestures ();
 		
 		do{
@@ -201,18 +155,14 @@ public class Instantiation : MonoBehaviour
 			print ("loopover");
 			
 			if (isCloneGenerationActive) {
-				print ("CreateClonesCalled");
-				CreateClones (hands);
+				print ("DynamicMeshGeneratorCalled");
+				DynamicMeshGenerator (hands);
 				print ("willGenerateClones");
 			} else {
-				print ("CreateClonesNOTCalled");
+				print ("DynamicMeshGeneratorNOTCalled");
 			}
 			print ("finalloopend"+isCloneGenerationActive);
 		} while(isCloneGenerationActive);
 		print ("wloopover"+isCloneGenerationActive);
 	}
-
-
-
-
 }
---
+++ Virtual Painting/Assets/Scripts/Movement.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+
+	//MouseControl
+	private float lookAngle;
+	float rotationGain;
+
+	void Start () {
+		rotationGain = 4.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//position
+		if(Input.GetKey("w")){
+			transform.Translate(Vector3.forward * Time.deltaTime);
+		} else if (Input.GetKey("a")){
+			transform.Translate(Vector3.left * Time.deltaTime);
+		} else if (Input.GetKey("s")){
+			transform.Translate(Vector3.back * Time.deltaTime);
+		} else if (Input.GetKey("d")){
+			transform.Translate(Vector3.right * Time.deltaTime);
+		}
+
+		//where you lookin' at?
+		float mouseXPosition = Input.GetAxis ("Mouse X");
+		lookAngle += mouseXPosition * rotationGain;
+		Quaternion rotation = Quaternion.AngleAxis (lookAngle, new Vector3 (0.0f, 1.0f, 0.0f));
+		transform.rotation = rotation;
+	}
+}
